01080fe3e094f1577683b7f4715608ea6f88552e
JSG-8579 <rkfntmdrhks@naver.com>
2023-12-27 18:16:24 +0900

    view 확인

--- js/view.js
+++ js/view.js
@@ -18,14 +18,12 @@
       // console.log(error);
     });
 
-
-  const elImg = document.querySelector('.view_img_1'),
+    const elImg = document.querySelector('.view_img_1'),
     elViewText1 = document.querySelector('.view_text_1'),
     elViewText2 = document.querySelector('.view_text_2'),
     elTextBox = document.querySelector('.text_box'),
     elExImg = document.querySelector('.img_ex');
-
-
+    
 
   let goodsView = function () {
 
@@ -35,7 +33,7 @@
     let c = '';
     let d = '';
     let e = '';
-    let z = goods[0];
+    let z = goods[7];
     const picture = z.product_img.split(',');
     for (j = 0; j < picture.length; j++) {
       figure += `<img src="../images/beer/${picture[j]}" alt=""></img>`
@@ -65,8 +63,7 @@
             </div>`
 
     e += `
-    <img src="../images/beer/${z.explain}" alt="">
-    `
+    <img src="../images/beer/${z.explain}" alt="">`
 
     elImg.innerHTML = a;
     elViewText1.innerHTML = b;
@@ -76,26 +73,26 @@
 
 
     const elMainImg = document.querySelector('.view_main_img'),
-      elSubImg = document.querySelectorAll('.view_sub_image img'),
-      elExpand = document.querySelector('.expand'),
-      elFold = document.querySelector('.fold'),
-      elInputText = document.querySelector('.input_text'),
-      elButton = document.querySelector('.button1');
-
+    elSubImg = document.querySelectorAll('.view_sub_image img'),
+    elExpand = document.querySelector('.expand'),
+    elFold = document.querySelector('.fold'),
+    elInputText = document.querySelector('.input_text'),
+    elBtnBuy = document.querySelector('.btn_buy');
 
     elExpand.onclick = function (e) {
       e.preventDefault();
       elExpand.style.display = 'none';
       elFold.style.display = 'block';
       elExImg.style.height = 'auto';
-
+  
     }
+  
     elFold.onclick = function (e) {
       e.preventDefault();
       elExpand.style.display = 'block';
       elFold.style.display = 'none';
       elExImg.style.height = '2051px';
-
+  
     }
 
     elSubImg.forEach(function (값, 키) {
@@ -103,31 +100,36 @@
         let g = 값.getAttribute('src');
         elMainImg.setAttribute('src', g);
       }
-
+  
     })
 
     elInputText.onclick = function () {
 
-      console.log(parseInt(z.discount.replaceAll(',', '')));
+      // console.log(parseInt(z.discount.replaceAll(',', '')));
       // parseInt 정수만 나오게 replaceAll(',','') ,지우기
       let TransNum = parseInt(z.discount.replaceAll(',', ''));
       let goodsSum = (elInputText.value * TransNum).toString()
         .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");;
       let f = ''
       f += `<p>총 합계 금액 <b>${goodsSum}원</b></p>`
-
       text3.innerHTML = f;
+      
     }
 
-    elButton.onclick = function () {
-      console.log(elInputText.value)
-      console.log(z.explain)
-    }
-
-
+    
   }
 
+  
 
+  
+
+  
+
+  
+
+  
+
+  
 
 
 
