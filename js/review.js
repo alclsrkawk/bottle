f16d197e6d6320ab8d2507ea5e2dcf2689b7a044
SSkkky <worte5633@gmail.com>
2023-12-27 12:09:00 +0900

    상품후기 드롭다운 수정, 후기 작성 팝업 추가

--- js/review.js
+++ js/review.js
@@ -1,23 +1,78 @@
-const dropdown = document.querySelector('.dropdown');
-const toggleButton = document.querySelector('.dropdown-toggle');
-const menu = document.querySelector('.dropdown-menu');
-const options = document.querySelectorAll('.dropdown-option');
-const nextButton = document.querySelector('.next-button');
+const selectBox = document.querySelector('.review-tab-right .selectBox'),
+    label = document.querySelector('.selectBox .label'),
+    optionList = document.querySelector('.selectBox .optionList'),
+    optionItem = document.querySelectorAll('.selectBox .optionItem');
 
-toggleButton.addEventListener('click', function () {
-    menu.classList.toggle('show');
+// 드롭다운 메뉴 시작
+label.addEventListener('click', function () {
+    optionList.classList.toggle('active');
+    selectBox.classList.toggle('active');
 })
 
-toggleButton.addEventListener('blur', function () {
-    menu.classList.toggle('show');
-})
-
-options.forEach(function (item) {
-    item.addEventListener('click', function (e){
-        const value = e.currentTarget.textContent.trim();
-        console.log(e.currentTarget)
-        toggleButton.textContent = value;
-        toggleButton.classList.add('selected');
-        nextButton.removeAttribute('disabled');
+optionItem.forEach(function (el) {
+    el.addEventListener('mouseover', function () {
+        el.classList.add('hover');
     })
+
+    el.addEventListener('mouseout', function () {
+        el.classList.remove('hover');
+    })
+
+    el.onclick = function () {
+        optionItem.forEach(function (el) {
+            el.classList.add('hover2')
+        })
+        el.removeEventListener('mouseover', function () {
+            el.classList.add('hover');
+        })
+        let innerText = el.innerText;
+        label.innerText = innerText;
+        optionList.classList.toggle('active');
+        selectBox.classList.toggle('active');
+        let setTime = function () {
+            optionItem.forEach(function (el) { el.classList.remove('hover2') })
+        }
+        setTimeout(setTime, 1000);
+    }
+}) // 드롭다운 메뉴 끝
+
+// 후기작성 팝업 시작
+const writeBtn = document.querySelector('.review-tab-right .write-btn'),
+    popCont = document.querySelector('.popup-container'),
+    reviewPop = document.querySelector('#review-popup'),
+    reviewPopBtn = document.querySelector('#review-popup .next-btn'),
+    reviewPopText = document.querySelector('#review-popup textarea'),
+    reviewNext = document.querySelector('.next-step'),
+    reviewNextBtn = document.querySelector('.next-step .close'),
+    closeBtn = document.querySelector('.close'),
+    popBg = document.querySelector('.main-section .popbg');
+
+writeBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    reviewPop.classList.remove('hidden');
+    popCont.classList.add('active');
+    popBg.classList.add('bg-active');
+
+    closeBtn.onclick = function () {
+        popCont.classList.remove('active');
+        popBg.classList.remove('bg-active');
+    }
+
+    reviewPopBtn.onclick = function (e) {
+        e.preventDefault();
+
+        let userReview = reviewPopText.value;
+        console.log(userReview)
+
+        reviewPop.classList.add('hidden');
+        reviewNext.classList.add('active');
+    }
+
+    reviewNextBtn.onclick = function (e) {
+        e.preventDefault();
+        popCont.classList.remove('active');
+        reviewNext.classList.remove('active');
+        popBg.classList.remove('bg-active');
+    }
 })
+
