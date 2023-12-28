f6db63e101fece4dbe1c5b9c002382c99d80c4d4
dnaals <swm8793@naver.com>
2023-12-28 11:47:37 +0900

    payment 반응형,js수정

--- js/payment.js
+++ js/payment.js
@@ -1,4 +1,5 @@
 const sameInfo = document.querySelector('#same');
+const receiverInfo = document.querySelector("#receiver_num");
 //이메일 직접입력 기능 시작
 order_email_address.onclick = function () {
     if (order_email_address.value == "direct") {
@@ -32,12 +33,14 @@ sameInfo.onclick = function () {
         } else {
             receiver_direct.style.display = "none";
         }
-    } else {
+    }
+    else {
         receiver_name.value = ""; //이름
         receiver_num.value = "";  //핸드폰번호
         receiver_tel.value = "010";  //핸드폰 앞번호
         receiver_email.value = ""; //이메일
         receiver_email_address.value = "naver.com";
+        
         if (receiver_email_address.value == "direct") {
             receiver_direct.style.display = "block";
         } else {
@@ -89,5 +92,27 @@ agree.onclick = function (e) {
 
 
 //결제하기버튼시작
+const payBtn = document.querySelector('.pay-button');
+payBtn.onclick = function(){
+    if(check_1.checked==false||check_2.checked==false||check_4.checked==false){
+        alert('이용약관(필수)에 동의해주세요');
+    } else if(order_name.value==""||order_num.value==""||order_email.value==""){
+        alert('주문자 정보를 확인해주세요')
+    } else if(receiver_name.value==""||receiver_num.value==""||receiver_email.value==""){
+        alert('수령자 정보를 확인해주세요');
+    } 
+    else if(zip_code.value==""||address_1.value==""||address_2.value==""){
+        alert('주소지를 확인해주세요');
+    } else if(order_email_address.value=='direct' && order_direct.value==""){
+        alert("주문자 이메일 주소를 확인해주세요");
+    } else if(receiver_email_address.value=='direct' && receiver_direct.value==""){
+        alert("수령자 이메일 주소를 확인해주세요");
+    }
+    else{
+        alert('결제가 완료되었습니다.');
+        location.href='./payment.html';
+    }
 
+    
+}
 //결제하기버튼끝
