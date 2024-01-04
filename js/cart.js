6e0014e72e44d730ee6aa8e689f0bbccd410fce4
dnaals <swm8793@naver.com>
2024-01-04 12:55:06 +0900

    장바구니 기능

--- js/cart.js
+++ js/cart.js
@@ -1,42 +1,5 @@
 const cartBtn = document.querySelector('button');
 let item = null;
-//상품 받아오기
-function getUrlParams() {
-    const params = {};
-    window.location.search.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (str, key, value) { params[key] = value; });
-    return params;
-}
-const params = getUrlParams();
-let goods = [];
-
-
-cartBtn.onclick = () => {
-    item1 =
-        {
-            "Idx": 6
-            , "name": "soju"
-            , "product_name": "asd"
-            , "price": "80,000원"
-            , "discount": "76,000원"
-            , "brand": "시나브로와이너리"
-            , "product_img": "wine5.png"
-        }
-
-    fnCart(item1)
-}
-const fnCart = (obj) => {
-    let newItem = JSON.parse(localStorage.getItem('products')) || [];
-    console.log("newItem :", newItem)
-
-    if (newItem != null) {
-        newItem.push(obj)
-        localStorage.setItem('products', JSON.stringify(newItem));
-    }
-    else {
-        localStorage.setItem('products', JSON.stringify(obj));
-    }
-
-}
 
 if (localStorage.getItem('products') == null) {
     list.innerHTML = '<p>삼품없음</p>';
@@ -48,13 +11,17 @@ if (localStorage.getItem('products') == null) {
 
     for (i = 0; i < JSON.parse(localData).length; i++) {
         let localArr = JSON.parse(localData)[i];
-        addList += `<div id="list">
+        let disCount =  (parseInt(localArr.discount.replaceAll(',', '')))*localArr.ea;
+        let price = (parseInt(localArr.price.replaceAll(',', '')))*localArr.ea;
+        console.log(parseInt(localArr.discount.replaceAll(',', '')))
+        console.log(localArr.ea)
+        addList += `<div id=list2>
             <div class="cart_1">
                 <li><img src="../images/${localArr.name}/${localArr.product_img}"></li>
             </div>
             <div class="cart_2">
                 <b>${localArr.product_name} <span>${localArr.brand}</span></b><br>
-                <p>${localArr.discount}<span>${localArr.price}</span></p>
+                <p>${disCount.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")+"원"}<span>${price.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")+"원"}</span></p>
                 <div>
                     수량 : ${localArr.ea}개
                 </div>
@@ -66,12 +33,17 @@ if (localStorage.getItem('products') == null) {
     list.innerHTML = addList;
 
     const delBtn = document.querySelectorAll('.delete');
+    newItem = JSON.parse(localStorage.getItem("products") || "[]");
     delBtn.forEach(function(btn,key){
         btn.onclick = function(){
-            console.log(JSON.parse(localData)[key]);
-            //console.log((localStorage.getItem('products'))[key]);
-            localStorage.removeItem(JSON.parse(localData)[key]);
+            newItem.splice(key,1);
+            localStorage.setItem("products",JSON.stringify(newItem));
+            location.reload()
         }
     });
+    all_del.onclick = function(){
+        localStorage.removeItem('products');
+        location.reload()
+    }
 }
 
